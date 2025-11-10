a92bb8721749d29f13b566827cbe71f95b6072c7
Ctrshift-Pm <pedromatheus.ch@gmail.com>
2025-11-10 19:29:22 -0300

    Consertando erro bobo

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import Login from './lib/Login.svelte';
   import Dashboard from './lib/Dashboard.svelte';
   import { Toaster } from '$lib/components/ui/sonner';
+  import 'svelte-sonner/style.css';
   import { authToken, theme } from './lib/store';
 
   $: {
