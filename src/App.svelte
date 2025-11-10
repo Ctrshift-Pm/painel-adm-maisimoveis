dda2f3f2b22e989c1f68b937290504003694822d
Ctrshift-Pm <pedromatheus.ch@gmail.com>
2025-11-10 19:32:27 -0300

    mudando brevemente o import svelte-sooner

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
   import Login from './lib/Login.svelte';
   import Dashboard from './lib/Dashboard.svelte';
   import { Toaster } from '$lib/components/ui/sonner';
-  import 'svelte-sonner/style.css';
+  import 'svelte-sonner/dist/style.css';
   import { authToken, theme } from './lib/store';
 
   $: {
