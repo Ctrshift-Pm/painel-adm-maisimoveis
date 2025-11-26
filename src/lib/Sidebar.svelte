67dd6cb4891808ff94d60ffd89f9320ee3d51348
Ctrshift-Pm <pedromatheus.ch@gmail.com>
2025-11-26 09:47:13 -0300

    diminuida a logo de tamanho

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -48,7 +48,7 @@
   class="fixed inset-y-0 left-0 z-30 w-64 bg-gray-800 dark:bg-gray-900 text-white flex flex-col transform transition-transform duration-300 ease-in-out lg:translate-x-0 {isOpen ? 'translate-x-0' : '-translate-x-full'}"
 >
   <div class="h-16 flex items-center justify-center border-b border-gray-700 dark:border-gray-800">
-    <img src={maisimoveis} alt="MaisImóveis" class="h-25 w-auto object-contain" loading="lazy" />
+    <img src={maisimoveis} alt="MaisImóveis" class="h-19 w-auto object-contain" loading="lazy" />
   </div>
   <nav class="flex-1 px-4 py-4 space-y-2">
     <button
