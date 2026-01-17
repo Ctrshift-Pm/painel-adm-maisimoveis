73e5a94016bc7df6f970aafa224941f1f6dbfc46
Ctrshift-Pm <pedromatheus.ch@gmail.com>
2026-01-17 17:22:27 -0300

    teste 2

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -48,7 +48,9 @@
   class="fixed inset-y-0 left-0 z-30 w-64 bg-gray-800 dark:bg-gray-900 text-white flex flex-col transform transition-transform duration-300 ease-in-out lg:translate-x-0 {isOpen ? 'translate-x-0' : '-translate-x-full'}"
 >
   <div class="h-16 flex items-center justify-center border-b border-gray-700 dark:border-gray-800">
-    <img src={encontreaquiimoveis} alt="encontreaquiimoveis" class="h-19 w-auto object-contain" loading="lazy" />
+    <div class="bg-white rounded-md p-2">
+      <img src={encontreaquiimoveis} alt="encontreaquiimoveis" class="h-19 w-auto object-contain" loading="lazy" />
+    </div>
   </div>
   <nav class="flex-1 px-4 py-4 space-y-2">
     <button
